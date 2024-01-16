2030352dd9ccb049c4232d4fafbaed80f1429bf5
Aditya Kaneriya <adityakaneriya1711@gmail.com>
2024-01-16 18:18:41 +0530

    Task-2 almost complited

---
+++ Task-2/script.js
@@ -0,0 +1,42 @@
+const leftArrow = document.getElementById("leftArrow");
+const rightArrow = document.getElementById("rightArrow");
+const bannerImage = document.getElementById("bannerImage");
+
+let cnt = 1;
+
+const newSrc = {
+  1: "./design-4/image/Banner-1.png",
+  2: "./design-4/image/Banner-2.png",
+  3: "./design-4/image/Banner-3.png",
+};
+
+const moveRight = () => {
+  cnt++;
+  if (cnt > Object.keys(newSrc).length) {
+    cnt = 1;
+  }
+  bannerImage.src = newSrc[cnt];
+
+  bannerImage.classList.add("addAnimationLeftToRight");
+
+  setTimeout(() => {
+    bannerImage.classList.remove("addAnimationLeftToRight");
+  }, 1000);
+};
+
+const moveLeft = () => {
+  cnt--;
+  if (cnt < 1) {
+    cnt = Object.keys(newSrc).length;
+  }
+  bannerImage.src = newSrc[cnt];
+
+  bannerImage.classList.add("addAnimationRightToLeft");
+
+  setTimeout(() => {
+    bannerImage.classList.remove("addAnimationRightToLeft");
+  }, 1000);
+};
+
+leftArrow.addEventListener("click", moveLeft);
+rightArrow.addEventListener("click", moveRight);
